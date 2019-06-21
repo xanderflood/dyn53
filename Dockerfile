5a09c551964b9fc4e8b7f1036e8396f3bd165d05
Xander Flood <xander.t.flood@gmail.com>
2019-06-20 23:04:52 -0400

    implement it

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine:3.9
+
+RUN apk add --no-cache curl python py-pip
+RUN pip install awscli --upgrade --user
+RUN mv /root/.local/bin/* /usr/local/bin
+RUN apk -v --purge del py-pip
+RUN curl -L https://github.com/sequenceiq/docker-alpine-dig/releases/download/v9.10.2/dig.tgz|tar -xzv -C /usr/local/bin/
+
+COPY update-record.sh /update-record.sh
+COPY entrypoint.sh /entrypoint.sh
+
+CMD ["/entrypoint.sh"]
+
---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+AWS_ACCESS_KEY_ID=`cat /run/secrets/aws_access_key_id`
+AWS_SECRET_ACCESS_KEY=`cat /run/secrets/aws_secret_access_key`
+
+# no idea why this is necessary, but AWS seems not to be able to see my credentials
+mkdir /root/.aws
+echo "[default]
+aws_access_key_id=$AWS_ACCESS_KEY_ID
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+" > /root/.aws/credentials
+
+echo "
+# m h  dom mon dow   command
+*/30 * * * * hostname=$HOSTNAME . /update-record.sh >> /var/log/dyn53.log
+" > /crontab.txt
+
+/usr/bin/crontab /crontab.txt
+
+# start cron
+/usr/sbin/crond -f -l 8
+
